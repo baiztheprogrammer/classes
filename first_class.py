8cfbc0f26366cab43b9a86bd942e32f8fb2a24ac
baiztheprogrammer <kyloren4206969@gmail.com>
2020-11-10 12:30:57 +0600

    classes

---
+++ first_class.py
@@ -0,0 +1,13 @@
+class Human:
+    def __init__(self,name):
+        self.name = name
+        self.age = 0
+
+    def change_age(self,num):
+            if num > 0:
+                self.age = num
+            else:
+                print('what?')
+new_human = Human(name='maksim')
+new_human1 = Human(name='emir')
+new_human1.change_age(-20)
