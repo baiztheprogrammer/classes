053cc20fa841a356cb65f62e1ac858680d3d4afa
baiztheprogrammer <kyloren4206969@gmail.com>
2020-11-12 10:42:03 +0600

    hungry

--- first_class.py
+++ first_class.py
@@ -4,10 +4,10 @@ class Human:
         self.age = 0
 
     def change_age(self,num):
-            if num > 0:
-                self.age = num
-            else:
-                print('what?')
+        if num > 0:
+            self.age = num
+        else:
+            print('what?')
 new_human = Human(name='maksim')
 new_human1 = Human(name='emir')
 new_human1.change_age(-20)
