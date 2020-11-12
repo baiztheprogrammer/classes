5c5b54b56f3049a3522752553773cf13dcc5eb1b
baiztheprogrammer <kyloren4206969@gmail.com>
2020-11-12 11:12:57 +0600

    mercury

---
+++ class_mercury.py
@@ -0,0 +1,20 @@
+class Planet:
+    def __init__(self,name,position):
+        self.name = name
+        self.form = 'shape'
+        self.position = position
+        self.temp = 0
+        self.size = 0
+
+    def description(self):
+        print(self.name,self.form,self.position,self.temp,self.size)
+
+class Mercury(Planet):
+    def __init__(self,position,name='Mercury'):
+        super().__init__(name,position)
+        self.temp = 120
+        self.size = 'small'
+
+class Jupiter(Planet):
+    def __init__(self,position,name='Jupiter'):
+        super().__init__(name,position)
