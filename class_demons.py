611cabd38e8c978a4198f3efc411bb667ed7d260
baiz(ak) <73326162+baiztheprogrammer@users.noreply.github.com>
2020-11-13 12:56:56 +0600

    Add files via upload
    unfinished pls dont touch lol

--- class_demons.py
+++ class_demons.py
@@ -1,38 +1,43 @@
 class Demon:
-    def __init__(self,name,size,type,home):
+    def __init__(self,name,size,type,home,health):
         self.name = name
         self.size = size
         self.type = type
         self.home = home
+        self.health = health
         self.wings = True
         self.horns = True
 
+
+    def attack_demon(self,damage):
+        result = 20 - damage
+        print (f"Demon has hit minion for {damage} damage!")
+        print(f'Minion health left: {result}')
+        if damage >= 20:
+            print('Minion has died, Demon victory!')
+
     def description(self):
-        print(f"Name:{self.name} - Size:{self.size} - Type:{self.type} - Home:{self.home} - Wings:{self.wings} - Horns:{self.horns}")
+        print(f"Name:{self.name} - Size:{self.size} - Type:{self.type} - Home:{self.home} - Health:{self.health} - Wings:{self.wings} - Horns:{self.horns}")
 
 
 class Minion(Demon):
-    def __init__(self,name,size,type,home):
-        self.name = name
-        self.size = size
-        self.type = type
-        self.home = home
-        self.tail = True
+    def __init__(self,name,size,type,home,health,tail):
+        super().__init__(name,size,type,home,health)
+        self.tail = tail
         self.wings = False
         self.horns = False
 
+    def attack_minion(self, damage):
+        result = doom.health - damage
+        print(f"Minion has hit Demon for {damage} damage!")
+        print(f'Demon health left: {result}')
+        if damage >= doom.health:
+            print(f'Demon has died,Minion victory!')
+
+
     def description(self):
-        print(f"Name:{self.name} - Size:{self.size} - Type:{self.type} - Home:{self.home} - Wings:{self.wings} - Horns:{self.horns} - Tail:{self.tail}")
+        print(f"Name:{self.name} - Size:{self.size} - Type:{self.type} - Home:{self.home} - Health:{self.health} - Tail:{self.tail} - Wings:{self.wings} - Horns:{self.horns}")
 
-    def sleep(self):
-        if sleep = True:
-
-
-
-
-
-baiz = Demon('doom','Tall','Hunter','Hell')
-baiz.description()
-minion = Minion('naix','Small','Passive','Forest')
-minion.description()
-
+doom = Demon('doom','Tall','Hunter','Hell',50)
+minion = Minion('naix','Small','Passive','Forest',20,'Long')
+minion.attack_minion(20)
